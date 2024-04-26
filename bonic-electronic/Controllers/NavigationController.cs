d9b74d7abcf3042228d4d057658563123d6d7f85
rahulvasava98 <rahulvasava52986@gmail.com>
2024-04-26 18:02:09 +0530

    created navigation

---
+++ bonic-electronic/Controllers/NavigationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bonic_electronic.Controllers
+{
+    public class NavigationController : Controller
+    {
+        public IActionResult navigation()
+        {
+            return View();
+        }
+    }
+}
