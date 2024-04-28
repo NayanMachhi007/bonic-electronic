7afcc9666cc4388947fa66b4c359c134585464e3
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-04-28 07:39:42 +0100

    new code update

---
+++ bonic-electronic/wwwroot/js/swiper.js
@@ -0,0 +1,14 @@
+﻿let swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+    let swipercustom = new swipercustonnew(".banner-swiper-custom", {
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+        },
+    });
