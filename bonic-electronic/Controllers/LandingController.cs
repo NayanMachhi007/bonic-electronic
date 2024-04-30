d284c6a7a03d1c51714b935c1831ebd154b781b7
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-04-30 12:26:05 +0100

    new code udpated now

---
+++ bonic-electronic/Controllers/LandingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bonic_electronic.Controllers
+{
+    public class LandingController : Controller
+    {
+        public IActionResult landingpage()
+        {
+            return View();
+        }
+    }
+}
